1028b7a0d9c0852cfdd93ab43183af7c93f1608f
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-11-03 21:01:26 +0100

    Removed pkg from package name.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="BoSS-pkg-Tomev",
+    name="BoSS-Tomev",
     version="0.0.1",
     author="Tomasz Rybotycki",
     author_email="rybotycki.tomasz+boss@gmail.com",
